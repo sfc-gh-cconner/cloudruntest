858c294fd6bc1362993ef5644911d65df382273c
Chris Conner <chris.conner@snowflake.com>
2023-12-15 16:07:00 -0500

    First commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:18.04
+RUN apt update && apt install -y software-properties-common
+RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt update && apt install -y python3.8 python3-distutils curl wget net-tools unzip zip vim
+RUN wget https://bootstrap.pypa.io/get-pip.py
+RUN python3.8 get-pip.py
+RUN pip3.8 install snowflake-connector-python
+RUN echo "2"
+COPY . /
+RUN mkdir /logs
+CMD ["python3.8", "/test.py"]
---
+++ test.py
@@ -0,0 +1,104 @@
+import os
+import snowflake.connector
+import logging
+
+for logger_name in ['snowflake','botocore']:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+        ch = logging.FileHandler('/logs/python_connector.log')
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(logging.Formatter('%(asctime)s - %(threadName)s %(filename)s:%(lineno)d - %(funcName)s() - %(levelname)s - %(message)s'))
+        logger.addHandler(ch)
+
+def get_con():
+    print("getting connection")
+    con = snowflake.connector.connect(
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        role='ACCOUNTADMIN',
+        warehouse='CC_WAREHOUSE',
+        session_parameters={
+            'QUERY_TAG': 'chrispythontest',
+        }
+    )
+    print("returning connection")
+    return con
+
+con = get_con()
+cur = con.cursor()
+print("Running first query")
+result = cur.execute("select c_custkey, c_name, c_address, c_nationkey, c_phone, c_acctbal, c_mktsegment, c_comment, row_number() over(order by c_custkey, c_name, c_address, c_nationkey, c_phone, c_acctbal, c_mktsegment, c_comment) as ROWNUM from snowflake_sample_data.tpch_sf100.customer")
+print("First query done")
+query_id = cur.sfqid
+print("Query ID:", query_id)
+
+cur.close()
+con.close()
+import time
+
+print("set 1")
+con = get_con()
+cur = con.cursor()
+result1 = cur.execute(f"select * from table(result_scan('{query_id}')) where rownum between 0 and 2000000")
+result1 = result1.fetchall()
+count = 1
+for row in result:
+    if count < 5:
+        print(row)
+
+    count = count + 1
+
+cur.close()
+con.close()
+
+print("set 2")
+con = get_con()
+cur = con.cursor()
+result2 = cur.execute(f"select * from table(result_scan('{query_id}')) where rownum between 2000001 and 4000000")
+result2 = result2.fetchall()
+count = 1
+for row in result:
+    if count < 5:
+        print(row)
+
+    count = count + 1
+
+cur.close()
+con.close()
+
+print("set 3")
+con = get_con()
+cur = con.cursor()
+result3 = cur.execute(f"select * from table(result_scan('{query_id}')) where rownum between 4000001 and 6000000")
+result3 = result3.fetchall()
+count = 1
+for row in result:
+    if count < 5:
+        print(row)
+
+    count = count + 1
+
+cur.close()
+con.close()
+
+
+time.sleep(600)
+print("set 4")
+con = get_con()
+cur = con.cursor()
+result4 = cur.execute(f"select * from table(result_scan('{query_id}')) where rownum between 6000001 and 8000000")
+result4 = result4.fetchall()
+count = 1
+for row in result:
+    if count < 5:
+        print(row)
+
+    count = count + 1
+
+cur.close()
+con.close()
+
+print("DONE")
+
+
